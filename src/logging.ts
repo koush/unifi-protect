5cb127e015c2ca3257881498b44eb376a53d0900
Koushik Dutta <koushd@gmail.com>
2021-08-23 19:24:11 -0700

    initial import from https://github.com/hjdhjd/homebridge-unifi-protect

---
+++ src/logging.ts
@@ -0,0 +1,31 @@
+/**
+ * Log levels to indicate importance of the logged message.
+ * Every level corresponds to a certain color.
+ *
+ * - INFO: no color
+ * - WARN: yellow
+ * - ERROR: red
+ * - DEBUG: gray
+ *
+ * Messages with DEBUG level are only displayed if explicitly enabled.
+ */
+export const enum LogLevel {
+    INFO = "info",
+    WARN = "warn",
+    ERROR = "error",
+    DEBUG = "debug",
+}
+
+/**
+ * Represents a logging device which can be used directly as a function (for INFO logging)
+ * but also has dedicated logging functions for respective logging levels.
+ */
+export interface Logging {
+
+    info(message: string, ...parameters: any[]): void;
+    warn(message: string, ...parameters: any[]): void;
+    error(message: string, ...parameters: any[]): void;
+    debug(message: string, ...parameters: any[]): void;
+    log(level: LogLevel, message: string, ...parameters: any[]): void;
+
+}
---
+++ src/settings.ts
@@ -0,0 +1,23 @@
+/* Copyright(C) 2017-2021, HJD (https://github.com/hjdhjd). All rights reserved.
+ *
+ * settings.ts: Settings and constants for homebridge-unifi-protect.
+ */
+
+// Number of API errors to accept before we implement backoff so we don't slam a Protect controller.
+export const PROTECT_API_ERROR_LIMIT = 10;
+
+// Interval, in seconds, to wait before trying to access the API again once we've hit the PROTECT_API_ERROR_LIMIT threshold.
+export const PROTECT_API_RETRY_INTERVAL = 300;
+
+// Protect API response timeout, in seconds. This should never be greater than 5 seconds.
+export const PROTECT_API_TIMEOUT = 3.5;
+
+// Heartbeat interval, in seconds, for the realtime Protect API on UniFI OS devices.
+// UniFi OS expects to hear from us every 15 seconds.
+export const PROTECT_EVENTS_HEARTBEAT_INTERVAL = 10;
+
+// FFmpeg highpass audio filter defaults - this setting attenuates (eliminates) frequencies below the value.
+export const PROTECT_FFMPEG_AUDIO_FILTER_HIGHPASS = 200;
+
+// How often, in seconds, should we refresh our Protect login credentials.
+export const PROTECT_LOGIN_REFRESH_INTERVAL = 1800;
