03fb6d01fabd461f2d560b7b006ed1ef2ab3ccc5
Koushik Dutta <koushd@gmail.com>
2021-08-23 23:25:50 -0700

    more cruft

--- src/logging.ts
+++ src/logging.ts
@@ -1,22 +1,4 @@
 /**
- * Log levels to indicate importance of the logged message.
- * Every level corresponds to a certain color.
- *
- * - INFO: no color
- * - WARN: yellow
- * - ERROR: red
- * - DEBUG: gray
- *
- * Messages with DEBUG level are only displayed if explicitly enabled.
- */
-export const enum LogLevel {
-    INFO = "info",
-    WARN = "warn",
-    ERROR = "error",
-    DEBUG = "debug",
-}
-
-/**
  * Represents a logging device which can be used directly as a function (for INFO logging)
  * but also has dedicated logging functions for respective logging levels.
  */
